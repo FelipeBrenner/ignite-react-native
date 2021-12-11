3ee16f66da8742ee5147b7ec10eebcf7f52012f3
felipe.brenner <felipe.brenner@cigam.com.br>
2021-12-11 20:04:55 -0300

    Criado projeto desafio todo complementar

---
+++ desafios/todo-complementar/README.md
@@ -0,0 +1,1 @@
+## 💻 [Notion](https://www.notion.so/Desafio-02-Adicionando-edi-o-e-alertas-800f949f3915496d8238031a8239124b)
---
+++ desafios/todo-complementar/src/pages/Home.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
+
+export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleAddTask(newTaskTitle: string) {
+    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((oldState) => [...oldState, data]);
+  }
+
+  function handleToggleTaskDone(id: number) {
+    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map((task) => ({ ...task }));
+
+    const foundTask = updatedTasks.find((task) => task.id === id);
+
+    if (!foundTask) return;
+
+    foundTask.done = !foundTask.done;
+
+    setTasks(updatedTasks);
+  }
+
+  function handleRemoveTask(id: number) {
+    //TODO - remove task from state
+    setTasks((oldState) => oldState.filter((task) => task.id !== id));
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header tasksCounter={tasks.length} />
+
+      <TodoInput addTask={handleAddTask} />
+
+      <TasksList
+        tasks={tasks}
+        toggleTaskDone={handleToggleTaskDone}
+        removeTask={handleRemoveTask}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#EBEBEB",
+  },
+});

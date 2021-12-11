ac31f1bb94b7a693ff481af36cd78f0fc7cd20f2
felipe.brenner <felipe.brenner@cigam.com.br>
2021-12-11 00:50:45 -0300

    Estruturado JSX e utilizado StyleSheet

---
+++ myskills/src/pages/Home.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
+
+export function Home() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Welcome, Brenner</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="New Skill"
+        placeholderTextColor="#555"
+      />
+      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+      <Text style={[styles.title, {marginTop: 20}]}>My Skills</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 50,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});

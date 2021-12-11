840625d0beb3769aa17486c05da0ad4e6ef92b71
felipe.brenner <felipe.brenner@cigam.com.br>
2021-12-11 20:50:02 -0300

    Implementado Alerts

--- desafios/todo-complementar/src/pages/Home.tsx
+++ desafios/todo-complementar/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
 
 import { Header } from "../components/Header";
 import { Task, TasksList } from "../components/TasksList";
@@ -9,10 +9,21 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const alreadyExists = tasks.find(
+      (title) => title.title === newTaskTitle.trim()
+    );
+
+    if (alreadyExists) {
+      Alert.alert(
+        "Task já cadastrada",
+        "Você não pode cadastrar uma task com o mesmo nome"
+      );
+      return;
+    }
+
     const data = {
       id: new Date().getTime(),
-      title: newTaskTitle,
+      title: newTaskTitle.trim(),
       done: false,
     };
 
@@ -33,8 +44,20 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-    setTasks((oldState) => oldState.filter((task) => task.id !== id));
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: "Não",
+        },
+        {
+          text: "Sim",
+          onPress: () =>
+            setTasks((oldState) => oldState.filter((task) => task.id !== id)),
+        },
+      ]
+    );
   }
 
   return (

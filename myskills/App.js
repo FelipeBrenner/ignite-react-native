e0fd136f336e2b6187f7fa2bd747b4ca25056e60
felipe.brenner <felipe.brenner@cigam.com.br>
2021-12-11 10:05:16 -0300

    Adicionado StatusBar

--- myskills/App.js
+++ myskills/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import {Platform, StatusBar} from 'react-native';
 import {Home} from './src/pages/Home';
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar
+        barStyle={Platform.OS === 'ios' ? 'light-content' : 'dark-content'}
+      />
+      <Home />
+    </>
+  );
 }

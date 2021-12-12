d554535287a8cdabf6f10436eea4d20a3b096570
felipe.brenner <felipe.brenner@cigam.com.br>
2021-12-12 16:16:34 -0300

    Criado componente Input

--- gofinances/App.tsx
+++ gofinances/App.tsx
@@ -7,7 +7,7 @@ import {
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
 import theme from "./src/global/styles/theme";
-import { Dashboard } from "./src/screens/Dashboard";
+import { Register } from "./src/screens/Register";
 import AppLoading from "expo-app-loading";
 
 export default function App() {
@@ -23,7 +23,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }

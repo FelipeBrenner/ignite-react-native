3ec1d620a7f8e6265940b294ab8d68c24f3c2f95
FelipeBrenner <felipeobrenner@hotmail.com>
2022-02-14 14:45:42 -0300

    Pagina Resumo e HistoryCard

--- gofinances/src/routes/app.routes.tsx
+++ gofinances/src/routes/app.routes.tsx
@@ -5,6 +5,7 @@ import { MaterialIcons } from "@expo/vector-icons";
 
 import { Dashboard } from "../screens/Dashboard";
 import { Register } from "../screens/Register";
+import { Resume } from "../screens/Resume";
 import theme from "../global/styles/theme";
 
 const { Navigator, Screen } = createBottomTabNavigator();
@@ -47,7 +48,7 @@ export function AppRoutes() {
       />
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: ({ size, color }) => (
             <MaterialIcons name="pie-chart" size={size} color={color} />

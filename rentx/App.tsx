e78983d62e4895dad4b2bfc1ca5e919c284a257d
FelipeBrenner <felipeobrenner@hotmail.com>
2022-02-18 16:31:04 -0300

    Interface Agendamento Concluído

--- rentx/App.tsx
+++ rentx/App.tsx
@@ -17,6 +17,7 @@ import theme from "./src/styles/theme";
 import { CarDetails } from "./src/screens/CarDetails";
 import { Scheduling } from "./src/screens/Scheduling";
 import { SchedulingDetails } from "./src/screens/SchedulingDetails";
+import { SchedulingComplete } from "./src/screens/SchedulingComplete";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -33,7 +34,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   );
 }

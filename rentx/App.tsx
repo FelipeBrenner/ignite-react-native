1b98bcf8d4250c15b63d7b11a992ee261f26d327
FelipeBrenner <felipeobrenner@hotmail.com>
2022-02-18 12:16:34 -0300

    Interface de agendamento

--- rentx/App.tsx
+++ rentx/App.tsx
@@ -15,6 +15,7 @@ import {
 import { Home } from "./src/screens/Home";
 import theme from "./src/styles/theme";
 import { CarDetails } from "./src/screens/CarDetails";
+import { Scheduling } from "./src/screens/Scheduling";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +32,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   );
 }
---
+++ rentx/src/screens/Scheduling/index.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useTheme } from "styled-components";
+import { BackButton } from "../../components/BackButton";
+
+import ArrowSvg from "../../assets/arrow.svg";
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  DateValueContainer,
+  Content,
+  Footer,
+} from "./styles";
+import { StatusBar } from "react-native";
+import { Button } from "../../components/Button";
+
+export function Scheduling() {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton onPress={() => {}} color={theme.colors.shape} />
+
+        <Title>
+          Escolha uma{"\n"}
+          data de início e{"\n"}
+          fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValueContainer selected={true}>
+              <DateValue>18/02/2022</DateValue>
+            </DateValueContainer>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValueContainer selected={false}>
+              <DateValue></DateValue>
+            </DateValueContainer>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+        <Footer>
+          <Button title="Confirmar" />
+        </Footer>
+      </Content>
+    </Container>
+  );
+}

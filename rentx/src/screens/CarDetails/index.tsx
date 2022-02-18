d6b53852862772c428f91f9e602e10994dc738a0
FelipeBrenner <felipeobrenner@hotmail.com>
2022-02-18 09:58:21 -0300

    Finalizado detalhes e acessórios

--- rentx/src/screens/CarDetails/index.tsx
+++ rentx/src/screens/CarDetails/index.tsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { Accessory } from "../../components/Accessory";
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header, CarImages } from "./styles";
+import speedSvg from "../../assets/speed.svg";
+import accelerationSvg from "../../assets/acceleration.svg";
+import forceSvg from "../../assets/force.svg";
+import gasolineSvg from "../../assets/gasoline.svg";
+import exchangeSvg from "../../assets/exchange.svg";
+import peopleSvg from "../../assets/people.svg";
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+} from "./styles";
 
 export function CarDetails() {
   return (
@@ -16,6 +38,36 @@ export function CarDetails() {
           imagesUrl={["https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png"]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 HP" icon={forceSvg} />
+          <Accessory name="Gasolina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={exchangeSvg} />
+          <Accessory name="2 pessoas" icon={peopleSvg} />
+        </Accessories>
+
+        <About>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          facilis, dolor assumenda voluptatum incidunt soluta a voluptate rerum
+          maxime repudiandae inventore quisquam impedit sapiente qui cumque
+          explicabo voluptatibus quis nulla!
+        </About>
+      </Content>
     </Container>
   );
 }

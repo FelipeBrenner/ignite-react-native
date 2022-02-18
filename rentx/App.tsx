93084a0f1525f1f5f90a728fa4f170a90b01f4cb
FelipeBrenner <felipeobrenner@hotmail.com>
2022-02-18 15:37:24 -0300

    Interface Detalhes do Agendamento

--- rentx/App.tsx
+++ rentx/App.tsx
@@ -16,6 +16,7 @@ import { Home } from "./src/screens/Home";
 import theme from "./src/styles/theme";
 import { CarDetails } from "./src/screens/CarDetails";
 import { Scheduling } from "./src/screens/Scheduling";
+import { SchedulingDetails } from "./src/screens/SchedulingDetails";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -32,7 +33,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Scheduling />
+      <SchedulingDetails />
     </ThemeProvider>
   );
 }
--- rentx/src/components/Accessory/styles.tsx
+++ rentx/src/components/Accessory/styles.tsx
@@ -2,7 +2,7 @@ import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  width: 109px;
+  width: 110px;
   height: 92px;
 
   justify-content: center;
@@ -18,4 +18,7 @@ export const Name = styled.Text`
   font-family: ${({ theme }) => theme.fonts.primary_500};
   color: ${({ theme }) => theme.colors.text};
   font-size: ${RFValue(13)}px;
+
+  line-height: 16px;
+  margin-top: 14px;
 `;

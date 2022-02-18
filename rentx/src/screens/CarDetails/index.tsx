57132ecf82549629ef40a37084d844c41366d7e6
FelipeBrenner <felipeobrenner@hotmail.com>
2022-02-18 09:27:05 -0300

    Continuado interface de detalhes do car

--- rentx/src/screens/CarDetails/index.tsx
+++ rentx/src/screens/CarDetails/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header } from "./styles";
+import { Container, Header, CarImages } from "./styles";
 
 export function CarDetails() {
   return (
@@ -9,6 +10,12 @@ export function CarDetails() {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={["https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png"]}
+        />
+      </CarImages>
     </Container>
   );
 }

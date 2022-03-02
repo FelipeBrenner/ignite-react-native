27bdde330d5867211bfcc3fd83f8431f0a596bed
FelipeBrenner <felipeobrenner@hotmail.com>
2022-03-01 22:18:14 -0300

    Acessibilidade do Form

--- rentx/src/screens/SignIn/index.tsx
+++ rentx/src/screens/SignIn/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  StatusBar,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { useTheme } from "styled-components";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
@@ -11,47 +16,51 @@ export function SignIn() {
   const theme = useTheme();
 
   return (
-    <Container>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Header>
-        <Title>Estamos{"\n"}quase lá.</Title>
-        <SubTitle>
-          Faça seu login para começar{"\n"}
-          uma experiência incrível.
-        </SubTitle>
-      </Header>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <Header>
+            <Title>Estamos{"\n"}quase lá.</Title>
+            <SubTitle>
+              Faça seu login para começar{"\n"}
+              uma experiência incrível.
+            </SubTitle>
+          </Header>
 
-      <Form>
-        <Input
-          iconName="mail"
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <PasswordInput iconName="lock" placeholder="Senha" />
-      </Form>
+          <Form>
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+            <PasswordInput iconName="lock" placeholder="Senha" />
+          </Form>
 
-      <Footer>
-        <Button
-          title="Login"
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
-        <Button
-          title="Criar conta gratuita"
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-          color={theme.colors.background_secundary}
-          light
-        />
-      </Footer>
-    </Container>
+          <Footer>
+            <Button
+              title="Login"
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            />
+            <Button
+              title="Criar conta gratuita"
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+              color={theme.colors.background_secundary}
+              light
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }

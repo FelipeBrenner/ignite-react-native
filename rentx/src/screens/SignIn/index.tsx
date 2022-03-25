f23ef2296fba27d94e4ea4acc8e77a45d92912b4
FelipeBrenner <felipeobrenner@hotmail.com>
2022-03-25 14:37:57 -0300

    Validação de formulário

--- rentx/src/screens/SignIn/index.tsx
+++ rentx/src/screens/SignIn/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import {
+  Alert,
   Keyboard,
   KeyboardAvoidingView,
   StatusBar,
   TouchableWithoutFeedback,
 } from "react-native";
+import * as Yup from "yup";
 import { useTheme } from "styled-components";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
@@ -17,6 +19,28 @@ export function SignIn() {
   const [password, setPassword] = useState("");
 
   const theme = useTheme();
+
+  async function handleSignIn() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido"),
+        password: Yup.string().required("A senha é obrigatória"),
+      });
+
+      await schema.validate({ email, password });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert("Opa", error.message);
+      } else {
+        Alert.alert(
+          "Erro na autenticação",
+          "Ocorreu um erro ao fazer login, verifique as credenciais"
+        );
+      }
+    }
+  }
 
   return (
     <KeyboardAvoidingView behavior="position" enabled>
@@ -54,12 +78,7 @@ export function SignIn() {
           </Form>
 
           <Footer>
-            <Button
-              title="Login"
-              onPress={() => {}}
-              enabled={false}
-              loading={false}
-            />
+            <Button title="Login" onPress={handleSignIn} loading={false} />
             <Button
               title="Criar conta gratuita"
               onPress={() => {}}

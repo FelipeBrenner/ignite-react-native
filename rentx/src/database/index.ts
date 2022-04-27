f20f4e3599862a93e9c494bb904d1ccb956520f0
FelipeBrenner <felipeobrenner@hotmail.com>
2022-04-27 15:58:04 -0300

    Criada estrutura da base de dados

---
+++ rentx/src/database/index.ts
@@ -0,0 +1,13 @@
+import { Database } from "@nozbe/watermelondb";
+import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
+import { User } from "./models/User";
+import { schemas } from "./schemas";
+
+const adapter = new SQLiteAdapter({
+  schema: schemas,
+});
+
+export const database = new Database({
+  adapter,
+  modelClasses: [User],
+});

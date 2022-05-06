2717c745fe0224bc8a9edd6135325a220246d57b
FelipeBrenner <felipeobrenner@hotmail.com>
2022-05-06 09:39:56 -0300

    Splash Screen

--- myskills/App.tsx
+++ myskills/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Platform, StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import {Home} from './src/pages/Home';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <StatusBar
